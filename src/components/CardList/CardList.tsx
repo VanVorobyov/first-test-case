b161bf770981d23d320a9c0bf63b8dfd1dd987ab
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2024-06-10 22:24:42 +0300

    feat(cardlist): add CardList component

---
+++ src/components/CardList/CardList.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react';
+import styles from './CardList.module.scss';
+import { useFetchCards } from '@hooks/useFetchCards.ts';
+import { Card } from '@components/Card/Card.tsx';
+import { ICard } from '@components/Card/types.ts';
+import { UNKNOWN_ERROR } from '@utils/constants.ts';
+
+export const CardList: FC = () => {
+	const { cards, loading, error } = useFetchCards();
+
+	if (loading) return <div>Loading...</div>;
+	if (error) return <div>{UNKNOWN_ERROR}</div>;
+	return (
+		<div className={styles.container}>
+			<ul className={styles.list}>
+				{cards.map((card: ICard) => (
+					<li className={styles.list_item}>
+						<Card key={card.id} {...card} />
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,13 +1,12 @@
-import { Card } from '@/components/Card/Card';
 import styles from './styles.module.scss';
+import { CardList } from '@components/CardList/CardList.tsx';
 
 export const Layout = () => {
 	return (
 		<>
 			<div className={styles.container}>
 				<h1>Карточки</h1>
-
-				<Card />
+				<CardList />
 			</div>
 		</>
 	);
